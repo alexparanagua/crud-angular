f876591a5336ac295a93cb7440123c78ed484a68
Alex Paranaguá <alexparanagua@gmail.com>
2023-01-20 18:21:37 -0300

    ChangesTo12

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -15,7 +15,7 @@ export class CoursesService {
 
   list(){
      return this.httpClient.get<Course[]>(this.API).pipe(
-      delay(2000),
+      //delay(2000),
       tap(courses => console.log(courses))
      );
   }
